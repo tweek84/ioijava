78a969aa2f8856031b749efb54a6ff3b141bf965
tjtoo <tjtoo@Killer-PC.litdom.lit.ie>
2020-01-18 15:38:42 +0000

    Version1

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,45 @@
+
+package ioi.java;
+
+/**
+
+ @author Conchraidh
+ */
+public class ApplicationMain
+{
+	/**
+	 @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+            String name="tj";
+            int age=11;
+            int number1=10;
+            int number2=5;
+            
+            int answer=number1*number2;
+                   
+                  System.out.println(name);
+                  System.out.println(age);
+                  System.out.println(answer);
+                  //Way 1
+                  System.out.println("My name is" + name +
+                          ".My age is" + age +
+                          ".My answer is "+ answer);
+                  //Way 2
+                 strin
+                          
+//		int counter = 1;
+//		while(counter < 5)
+//		{
+//			System.out.println("Number: " + counter);
+//			counter = counter + 1;
+//		}
+		//System.out.println("1");
+		//System.out.println("2");
+		//System.out.println("3");
+		//System.out.println("4");
+		// TODO code application logic here
+	}
+
+}
